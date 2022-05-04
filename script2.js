b65f1bd5999bd2e2f312c55c6375318467edb440
Kumneger0 <93655689+Kumneger0@users.noreply.github.com>
2022-05-04 16:14:37 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,5 +1,3 @@
-var score = 0;
-
 const quizData = [{
   question: "From the following which one has the highest life expectancy ?",
   a: "USA",
@@ -120,74 +118,28 @@ const quizData = [{
     d: "Cartagena",
     correct: "c",
   },];
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const nextButton = document.getElementById("submit");
+  
 
-let currentQuiz = 0;
-
-loadQuiz();
-
-// Function that loads question
-function loadQuiz() {
-  // First Deselect Default Option
-  deselectAnswer();
-
-  const currentQuizData = quizData[currentQuiz];
-
-  questionEl.innerText = currentQuizData.question; // Main Question
-  a_text.innerText = currentQuizData.a; // Option a
-  b_text.innerText = currentQuizData.b; // Option b
-  c_text.innerText = currentQuizData.c; // Option c
-  d_text.innerText = currentQuizData.d; // Option d
-}
-
-// Deselection Default Option
-function deselectAnswer() {
-  answerEls.forEach((answerEl) => {
-    answerEl.checked = false;
-  });
-}
-
-// Get Selected Option
-const getSelected = () => {
-  let answer;
-
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
-    }
-  });
-
-  return answer;
-};
-
-nextButton.addEventListener("click", () => {
-  // Check to see the answer
-  const answer = getSelected();
-
-  if (answer) {
-    if (answer == quizData[currentQuiz].correct) {
-      score++;
-    }
+var k = 0;
+function show(){
+  if(k==quizData.length){
+    alert("That's all")
+    location.reload();
   }
-  // Increment Upon Click
-  currentQuiz++;
-
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    quiz.innerHTML = `
-    <h3>You answered ${score} out of ${quizData.length} correctly.</h3>
-    <button onclick="location.reload()">Retake Quiz!</button> 
-    <a href = "answers.html"id= "answer"><button class= "answers">view correct answers</button></a>`;
-  }
-});
-function fed(){
-  alert('Thank you for your feedback!')
+  else if(k!=quizData.length){
+k++;
+var questions = document.querySelector("#ques");
+var option_a = document.querySelector("#choice-a");
+var option_b = document.querySelector("#choice-b");
+var option_c = document.querySelector("#choice-c");
+var option_d = document.querySelector("#choice-d");
+var correct = document.querySelector("#correct");
+questions.innerText = quizData[k].question;
+option_a.innerText = quizData[k].a;
+option_b.innerText = quizData[k].b;
+option_c.innerText = quizData[k].c;
+option_d.innerText = quizData[k].d;
+correct.innerText = quizData[k].correct;
 }
+}
+show();
